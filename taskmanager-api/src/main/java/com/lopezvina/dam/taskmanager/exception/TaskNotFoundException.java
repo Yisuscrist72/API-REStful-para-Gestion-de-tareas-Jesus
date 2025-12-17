0a309316c34c82e829700406a417c3161c5b6396
Yisuscrist72 <131564558+Yisuscrist72@users.noreply.github.com>
2025-12-17 13:34:41 +0000

    Delete taskmanager-api directory

--- taskmanager-api/src/main/java/com/lopezvina/dam/taskmanager/exception/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lopezvina.dam.taskmanager.exception;
-
-public class TaskNotFoundException extends RuntimeException {
-    public TaskNotFoundException(String id) {
-        super("No se encontró la tarea con ID: " + id);
-    }
-}
